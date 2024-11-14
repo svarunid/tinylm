6091d07ccdd4631488dd6d2c18af1ff0e98445d9
Arun S V <svarunid@gmail.com>
2024-11-14 15:54:12 +0530

    bug fixes & code refactoring

--- tinylm/models/gpt.py
+++ tinylm/models/gpt.py
@@ -1,87 +1,83 @@
+from functools import partial
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-def _get_activation(config):
-    params = config["activation"].get("parameters", {})
+def _get_layer(config, layers):
+    name = config["name"]
+    params = config.get("parameters", {})
 
-    match config["activation"]["name"].lower():
-        case "gelu":
-            return nn.GELU(**params)
-        case "relu":
-            return nn.ReLU()
-        case "leakyrelu":
-            return nn.LeakyReLU(**params)
-        case "prelu":
-            return nn.PReLU(**params)
-        case "silu":
-            return nn.SiLU()
-        case "glu":
-            return nn.GLU(**params)
+    if name in layers:
+        return partial(getattr(nn, name), **params)
+    else:
+        raise ValueError(f"Unknown activation function: {name}. The name is case-sensitive.")
+
+
+_get_norm = partial(_get_layer, layers=["LayerNorm", "RMSNorm"])
+_get_activation = partial(_get_layer, layers=["ReLU", "LeakyReLU", "PReLU", "SiLU", "GLU", "GELU"])
 
 
 class PositionalEmbedding(nn.Module):
-    def __init__(self, config):
+    def __init__(self, dim, seq, theta, name="absolute"):
         super().__init__()
-        self.config = config
-
-        name = config["positional"]["name"]
-        seq, emb = config["seq"], config["emb"]
-        theta = config["positional"]["parameters"].get("theta", 10000)
-
-        position = torch.arange(0, seq).unsqueeze(-1).expand(-1, emb)
-        position = position / torch.pow(theta, 2 * (torch.arange(0, emb) // 2) / emb)
+        self.dim, self.name = dim, name
+        position = torch.arange(0, seq).unsqueeze(-1).expand(-1, dim)
+        position = position / torch.pow(theta, 2 * (torch.arange(0, dim) // 2) / dim)
         if name == "absolute":
             position[:, 0::2] = torch.sin(position[:, 0::2])
             position[:, 1::2] = torch.cos(position[:, 1::2])
-            self.register_buffer("embedding", position)
-        elif name == "rotatory":
+            self.register_buffer("emb", position)
+        elif name == "rotary":
             sin, cos = torch.sin(position[:, 0::2]), torch.cos(position[:, 1::2])
-            sin = torch.stack([sin, sin], dim=-1).reshape(seq, emb)
-            cos = torch.stack([cos, cos], dim=-1).reshape(seq, emb)
-            self.register_buffer("embedding", torch.cat([sin, cos], dim=-1))
+            sin = torch.stack([sin, sin], dim=-1).reshape(seq, dim)
+            cos = torch.stack([cos, cos], dim=-1).reshape(seq, dim)
+            self.register_buffer("emb", torch.cat([sin, cos], dim=-1))
         else:
             raise ValueError(f"Unknown positional encoding type: {name}")
 
     def forward(self, x):
-        if self.config["positional"]["name"] == "absolute":
-            return x + self.embedding
+        if self.name == "absolute":
+            return x + self.emb
         else:
-            sentinel = self.config["emb"] // 2 + self.config["emb"] % 2
-            sin, cos = self.embedding[:, :sentinel], self.embedding[:, sentinel:]
-            x = x * cos + torch.stack([-x[..., 1::2], x[..., ::2]], dim=-1).reshape_as(x) * sin
+            sentinel = self.dim // 2 + self.dim % 2
+            sin, cos = self.emb[:, :sentinel], self.emb[:, sentinel:]
+            return x * cos + torch.stack([-x[..., 1::2], x[..., ::2]], dim=-1).reshape(x.size()) * sin
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, config):
+    def __init__(self, dim, heads, kv_heads=None):
         super().__init__()
-        assert config["emb"] % config["heads"] == 0
-        self.config = config
-        self.num_heads = config["heads"]
-        self.head_dim = config["emb"] // config["heads"]
+        assert dim % heads == 0
+        self.num_heads = heads
+        self.head_dim = dim // heads
 
-        self.query = nn.Linear(config["emb"], config["emb"])
-
-        self.enable_gqa = "gqa" in config
-        if self.enable_gqa:
-            self.kv_heads = config["gqa"]["kv_heads"]
-            self.key = nn.Linear(config["emb"], self.kv_heads * self.head_dim)
-            self.value = nn.Linear(config["emb"], self.kv_heads * self.head_dim)
+        if kv_heads is not None:
+            assert heads % kv_heads == 0
+            self.enable_gqa = True
+            self.kv_heads = kv_heads
         else:
-            self.key = nn.Linear(config["emb"], config["emb"])
-            self.value = nn.Linear(config["emb"], config["emb"])
+            self.enable_gqa = False
+            self.kv_heads = heads
 
-        self.proj = nn.Linear(config["emb"], config["emb"])
+        self.query = nn.Linear(dim, dim)
+        self.key = nn.Linear(dim, self.kv_heads * self.head_dim)
+        self.value = nn.Linear(dim, self.kv_heads * self.head_dim)
+        self.proj = nn.Linear(dim, dim)
 
-    def forward(self, x):
+    def forward(self, x, mask=None, pos_emb=None):
         B, T, C = x.size()
 
-        q = self.query(x).view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
-        k = self.key(x).view(B, T, self.num_heads, self.kv_heads if self.enable_gqa else self.head_dim).transpose(1, 2)
-        v = self.value(x).view(B, T, self.num_heads, self.kv_heads if self.enable_gqa else self.head_dim).transpose(1, 2)
+        q, k, v = self.query(x), self.key(x), self.value(x)
+        if pos_emb:
+            q, k = pos_emb(q), pos_emb(k)
 
-        attn_output = F.scaled_dot_product_attention(q, k, v, scale=self.head_dim**0.5, enable_gqa=self.enable_gqa)
+        q = q.view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
+        k = k.view(B, T, self.kv_heads, self.head_dim).transpose(1, 2)
+        v = v.view(B, T, self.kv_heads, self.head_dim).transpose(1, 2)
+
+        attn_output = F.scaled_dot_product_attention(q, k, v, attn_mask=mask, enable_gqa=self.enable_gqa)
         attn_output = attn_output.transpose(1, 2).contiguous().view(B, T, C)
 
         return self.proj(attn_output)
@@ -90,16 +86,18 @@ class MultiHeadAttention(nn.Module):
 class TransformerBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.ln_1 = nn.LayerNorm(config["emb"])
-        self.attn = MultiHeadAttention(config)
-        self.fc1 = nn.Linear(config["emb"], config["hidden_size"])
-        self.fc2 = nn.Linear(config["hidden_size"], config["emb"])
-        self.act = _get_activation(config)
-        self.ln_2 = nn.LayerNorm(config["emb"])
+        self.ln_1 = _get_norm(config["norm"])(config["dim"])
+        kv_heads = config["gqa"]["kv_heads"] if "gqa" in config else None
+        self.attn = MultiHeadAttention(config["dim"], config["heads"], kv_heads)
 
-    def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.fc2(self.act(self.fc1(x)))
+        self.fc1 = nn.Linear(config["dim"], config["hidden_size"])
+        self.fc2 = nn.Linear(config["hidden_size"], config["dim"])
+        self.act = _get_activation(config["activation"])()
+        self.ln_2 = _get_norm(config["norm"])(config["dim"])
+
+    def forward(self, x, mask=None, pos_emb=None):
+        x = x + self.attn(self.ln_1(x), mask, pos_emb)
+        x = x + self.fc2(self.act(self.fc1(self.ln_2(x))))
         return x
 
 
@@ -107,25 +105,29 @@ class GPT(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.embedding = nn.Embedding(config["vocab_size"], config["emb"])
-        self.blocks = nn.ModuleList([TransformerBlock(config) for _ in range(config["layers"])])
-        self.ln_f = nn.LayerNorm(config["emb"])
-        self.output_proj = nn.Linear(config["emb"], config["vocab_size"], bias=False)
+        dim, seq, vocab, positional = config["dim"], config["seq"], config["vocab_size"], config["positional"]
 
-        if config["positional"]["name"] == "absolute":
-            self.pos_embedding = PositionalEmbedding(config)
+        self.tok_embedding = nn.Embedding(vocab, dim)
+        self.pos_embedding = PositionalEmbedding(dim, seq, positional.get("theta", 10000), positional["name"])
+
+        self.blocks = nn.ModuleList([TransformerBlock(config) for _ in range(config["layers"])])
+        self.ln_f = _get_norm(config["norm"])(dim)
+        self.output_proj = nn.Linear(dim, vocab, bias=False)
 
         if config["tie_weights"]:
-            self.output_proj.weight = self.token_embedding.weight
+            self.output_proj.weight = self.tok_embedding.weight
 
-    def forward(self, x):
+    def forward(self, x, mask=None):
         B, T = x.size()
-        x = self.embedding(x)
+        x = self.tok_embedding(x)
 
+        pos_emb = None
         if self.config["positional"]["name"] == "absolute":
             x = self.pos_embedding(x)
+        else:
+            pos_emb = self.pos_embedding
 
         for block in self.blocks:
-            x = block(x)
+            x = block(x, mask=mask, pos_emb=pos_emb)
 
         return self.output_proj(self.ln_f(x))
