cbeec741ecf72367d10e511fa855c87d4dd0570a
Arun S V <svarunid@gmail.com>
2024-11-16 16:29:19 +0530

    bug fixes & code refactoring

--- tinylm/models/gpt.py
+++ tinylm/models/gpt.py
@@ -1,39 +1,30 @@
-from functools import partial
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-def _get_layer(config, layers):
-    name = config["name"]
-    params = config.get("parameters", {})
-
-    if name in layers:
-        return partial(getattr(nn, name), **params)
-    else:
-        raise ValueError(f"Unknown activation function: {name}. The name is case-sensitive.")
-
-
-_get_norm = partial(_get_layer, layers=["LayerNorm", "RMSNorm"])
-_get_activation = partial(_get_layer, layers=["ReLU", "LeakyReLU", "PReLU", "SiLU", "GLU", "GELU"])
+from tinylm import utils
 
 
 class PositionalEmbedding(nn.Module):
     def __init__(self, dim, seq, theta, name="absolute"):
         super().__init__()
         self.dim, self.name = dim, name
+        # Create a tensor of shape (seq, dim) with positional indices
         position = torch.arange(0, seq).unsqueeze(-1).expand(-1, dim)
+        # Compute the positional encodings
         position = position / torch.pow(theta, 2 * (torch.arange(0, dim) // 2) / dim)
         if name == "absolute":
             position[:, 0::2] = torch.sin(position[:, 0::2])
             position[:, 1::2] = torch.cos(position[:, 1::2])
             self.register_buffer("emb", position)
         elif name == "rotary":
+            # Compute sine and cosine components for rotary embeddings
             sin, cos = torch.sin(position[:, 0::2]), torch.cos(position[:, 1::2])
+            # Duplicate and reshape to match the required dimensions
             sin = torch.stack([sin, sin], dim=-1).reshape(seq, dim)
             cos = torch.stack([cos, cos], dim=-1).reshape(seq, dim)
-            self.register_buffer("emb", torch.cat([sin, cos], dim=-1))
+            self.register_buffer("sin", sin)
+            self.register_buffer("cos", cos)
         else:
             raise ValueError(f"Unknown positional encoding type: {name}")
 
@@ -41,13 +32,11 @@ class PositionalEmbedding(nn.Module):
         if self.name == "absolute":
             return x + self.emb
         else:
-            sentinel = self.dim // 2 + self.dim % 2
-            sin, cos = self.emb[:, :sentinel], self.emb[:, sentinel:]
-            return x * cos + torch.stack([-x[..., 1::2], x[..., ::2]], dim=-1).reshape(x.size()) * sin
+            return x * self.cos + torch.stack([-x[..., 1::2], x[..., ::2]], dim=-1).reshape_as(x) * self.sin
 
 
-class MultiHeadAttention(nn.Module):
-    def __init__(self, dim, heads, kv_heads=None):
+class CausalAttention(nn.Module):
+    def __init__(self, dim, heads, kv_heads=None, bias=False):
         super().__init__()
         assert dim % heads == 0
         self.num_heads = heads
@@ -61,43 +50,52 @@ class MultiHeadAttention(nn.Module):
             self.enable_gqa = False
             self.kv_heads = heads
 
-        self.query = nn.Linear(dim, dim)
-        self.key = nn.Linear(dim, self.kv_heads * self.head_dim)
-        self.value = nn.Linear(dim, self.kv_heads * self.head_dim)
-        self.proj = nn.Linear(dim, dim)
+        self.q_proj = nn.Linear(dim, dim, bias)
+        self.k_proj = nn.Linear(dim, self.kv_heads * self.head_dim, bias)
+        self.v_proj = nn.Linear(dim, self.kv_heads * self.head_dim, bias)
+        self.o_proj = nn.Linear(dim, dim)
 
     def forward(self, x, mask=None, pos_emb=None):
         B, T, C = x.size()
 
-        q, k, v = self.query(x), self.key(x), self.value(x)
+        q, k, v = self.q_proj(x), self.k_proj(x), self.v_proj(x)
         if pos_emb:
+            # If provide, apply rotary positional embeddings
             q, k = pos_emb(q), pos_emb(k)
 
         q = q.view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
         k = k.view(B, T, self.kv_heads, self.head_dim).transpose(1, 2)
         v = v.view(B, T, self.kv_heads, self.head_dim).transpose(1, 2)
 
-        attn_output = F.scaled_dot_product_attention(q, k, v, attn_mask=mask, enable_gqa=self.enable_gqa)
+        causal_mask = torch.tril(torch.ones(T, T, device=x.device))
+        mask = mask * causal_mask if mask is not None else causal_mask
+
+        attn_output = F.scaled_dot_product_attention(q, k, v, attn_mask=mask.bool(), enable_gqa=self.enable_gqa)
         attn_output = attn_output.transpose(1, 2).contiguous().view(B, T, C)
 
-        return self.proj(attn_output)
+        return self.o_proj(attn_output)
 
 
-class TransformerBlock(nn.Module):
+class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.ln_1 = _get_norm(config["norm"])(config["dim"])
+        dim = config["dim"]
+        norm, activation = config["norm"], config["activation"]
         kv_heads = config["gqa"]["kv_heads"] if "gqa" in config else None
-        self.attn = MultiHeadAttention(config["dim"], config["heads"], kv_heads)
 
-        self.fc1 = nn.Linear(config["dim"], config["hidden_size"])
-        self.fc2 = nn.Linear(config["hidden_size"], config["dim"])
-        self.act = _get_activation(config["activation"])()
-        self.ln_2 = _get_norm(config["norm"])(config["dim"])
+        # Dynamically initialize the input normalization layer based on configuration
+        self.input_norm = getattr(utils, norm["name"])(dim, **norm.get("parameters", {}))
+        self.attn = CausalAttention(dim, config["heads"], kv_heads, config["qkv_bias"])
+        self.attn_norm = getattr(utils, norm["name"])(dim, **norm.get("parameters", {}))
+
+        self.up_proj = nn.Linear(dim, config["hidden_size"])
+        # Dynamically initialize the activation function
+        self.act = getattr(utils, activation["name"])(**activation.get("parameters", {}))
+        self.down_proj = nn.Linear(config["hidden_size"], dim)
 
     def forward(self, x, mask=None, pos_emb=None):
-        x = x + self.attn(self.ln_1(x), mask, pos_emb)
-        x = x + self.fc2(self.act(self.fc1(self.ln_2(x))))
+        x = x + self.attn(self.input_norm(x), mask, pos_emb)
+        x = x + self.down_proj(self.act(self.up_proj(self.attn_norm(x))))
         return x
 
 
@@ -107,27 +105,29 @@ class GPT(nn.Module):
         self.config = config
         dim, seq, vocab, positional = config["dim"], config["seq"], config["vocab_size"], config["positional"]
 
-        self.tok_embedding = nn.Embedding(vocab, dim)
-        self.pos_embedding = PositionalEmbedding(dim, seq, positional.get("theta", 10000), positional["name"])
+        self.tok_emb = nn.Embedding(vocab, dim)
+        # Positional embedding layer (absolute or rotary based on configuration)
+        self.pos_emb = PositionalEmbedding(dim, seq, positional["parameters"]["theta"], positional["name"])
 
-        self.blocks = nn.ModuleList([TransformerBlock(config) for _ in range(config["layers"])])
-        self.ln_f = _get_norm(config["norm"])(dim)
+        self.blocks = nn.ModuleList([Block(config) for _ in range(config["layers"])])
+        self.norm = getattr(utils, config["norm"]["name"])(dim, **config["norm"].get("parameters", {}))
         self.output_proj = nn.Linear(dim, vocab, bias=False)
 
         if config["tie_weights"]:
-            self.output_proj.weight = self.tok_embedding.weight
+            self.output_proj.weight = self.tok_emb.weight
 
     def forward(self, x, mask=None):
         B, T = x.size()
-        x = self.tok_embedding(x)
+        x = self.tok_emb(x)
 
         pos_emb = None
         if self.config["positional"]["name"] == "absolute":
-            x = self.pos_embedding(x)
+            x = self.pos_emb(x)
         else:
-            pos_emb = self.pos_embedding
+            # For rotary embeddings, pass the positional embedding layer to the blocks
+            pos_emb = self.pos_emb
 
         for block in self.blocks:
             x = block(x, mask=mask, pos_emb=pos_emb)
 
-        return self.output_proj(self.ln_f(x))
+        return self.output_proj(self.norm(x))
